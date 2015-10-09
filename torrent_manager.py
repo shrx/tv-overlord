b557cb4fb8092260f16e6c6a14a75fdb74df7939
Sheldon McGrandle <developer@8cylinder.com>
2015-10-09 13:34:34 -0400

    test code

--- torrent_manager.py
+++ torrent_manager.py
@@ -21,7 +21,7 @@ import logging as l
 # set up logging
 this_file = os.path.splitext(os.path.basename(__file__))[0]
 log_file = '{}.log'.format(this_file)
-l.basicConfig(format='%(asctime)s -- %(levelname)s -- %(message)s',
+l.basicConfig(format='%(asctime)s::%(levelname)s::%(message)s',
               datefmt='%Y-%m-%d %H:%M:%S',
               filename=log_file)
 
@@ -57,3 +57,38 @@ def main(args):
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.1')
     main(args)
+
+'''
+[
+  [
+    ["Heroes Reborn S01E04", "https://extratorrent.unblocked.la/rss.xml?type=search&cid=0&search=Heroes%20Reborn%20S01E04"],
+    ["Name", "Size", "Date", "Seeds"],
+    [0, 10, 12, 6],
+    ["<", ">", "<", ">"]
+  ],
+  [
+    ["Heroes.Reborn.S01E04.HDTV.x264-KILLERS[ettv]", "306.75 MB", "Oct 09/2015", "39239", 0],
+
+    ["Heroes.Reborn.S01E04.720p.HDTV.x264-KILLERS[EtHD]", "978.02 MB", "Oct 09/2015", "3242", 1],
+
+    ["Heroes.Reborn.S01E04.HDTV.XviD-FUM[ettv]", "347.82 MB", "Oct 09/2015", "421", 2],
+
+    ["Heroes.Reborn.S01E04.720p.HDTV.x264-KILLERS", "978.02 MB", "Oct 09/2015", "120", 3],
+
+    ["Heroes Reborn S01E04 720p HDTV x265 HEVC 185MB - ShAaNiG", "184.98 MB", "Oct 09/2015", "22", 4],
+
+    ["Heroes Reborn S01E04 720p HDTV x264-KILLERS", "978.03 MB", "Oct 09/2015", "1", 5]
+  ]
+]
+
+[[["Lifes%20Too%20Short%20S01E01", "http://thepiratebay.se/search/ Lifes%20Too%20Short%20S01E01/0/7/0 Lifes%20Too%20Short%201x01/0/7/0 "], ["Name", "Size", "Date", "Seeds"], [0, 11, 12, 6], ["<", ">", "<", ">"]],
+[["Lifes Too Short S01E01 HDTV XviD-RiVER [eztv]", "232.78\u00a0MiB", "11-11\u00a02011", "11", 0], ["Lifes.Too.Short.S01E01.HDTV.XviD-RiVER", "242.12\u00a0MiB", "11-10\u00a02011", "1", 1], ["Lifes.Too.Short.S01E01.HDTV.Subtitulado.Esp.SC.avi", "232.99\u00a0MiB", "04-01\u00a02013", "1", 2], ["Lifes Too Short S01E01 HDTV XviD-RiVER", "232.78\u00a0MiB", "11-11\u00a02011", "0", 3], ["Lifes Too Short S01E01 HDTV XviD-RiVER[ettv]", "232.78\u00a0MiB", "11-11\u00a02011", "0", 4]]]
+
+[[["Lifes Too Short S01E03", "https://extratorrent.unblocked.la/rss.xml?type=search&cid=0&search=Lifes%20Too%20Short%20S01E03"], ["Name", "Size", "Date", "Seeds"], [0, 10, 12, 6], ["<", ">", "<", ">"]],
+[
+["Lifes Too Short 1x08 HDTV x264-FoV [eztv]", "423.44 MB", "Mar 31/2013", "36", 0],
+["Lifes Too Short S01E01 HDTV XviD-RiVER[ettv]", "232.78 MB", "Nov 11/2011", "0", 1],
+["Lifes Too Short for Tantric Sex 50 Shortcuts to Sexual Ecstasy by Kate Taylor", "39.83 KB", "Feb 03/2009", "0", 2]
+]
+]
+'''
