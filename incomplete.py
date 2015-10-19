951c1b6d5eabb587d45c543b16fc46e58ce94fe3
Sheldon McGrandle <developer@8cylinder.com>
2015-10-19 16:06:06 -0400

    Add deluge_done.py and a temporary helper script to list incomplete downloads

---
+++ incomplete.py
@@ -0,0 +1,31 @@
+
+import os
+import re
+import sqlite3
+import time
+import datetime
+import sys
+from pprint import pprint as pp
+import logging
+from tv_config import Config
+from DB import DB
+from notify import Tell
+import shutil
+
+
+def main():
+    sql = '''SELECT show_title, chosen_hash FROM tracking WHERE complete is NULL'''
+    db = DB()
+    unfinished = db.run_sql(sql)
+
+    debug_command = '''export TR_TORRENT_NAME='%s'; export TR_TORRENT_DIR='%s'; export TR_TORRENT_HASH='%s'; python ~/projects/media-downloader/src/transmission_done.py'''
+    for show in unfinished:
+        print
+        print show[0]
+        print '-' * len(show[0])
+        command = debug_command % ('', '/home/sm/Shows', show[1])
+        print command
+
+if __name__ == '__main__':
+    main()
+
