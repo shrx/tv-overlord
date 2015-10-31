1ac790e19dfe233d20e1ce37abca4b2054383b37
Sheldon McGrandle <developer@8cylinder.com>
2015-10-31 12:21:51 -0400

    Remove the 'data' column from the tracking table
    Saving the search data array serves no purpose.

--- tv/tracking.py
+++ tv/tracking.py
@@ -23,9 +23,6 @@ class Tracking(DB):
 
     def save(self, show_title, season, episode, data, chosen_url):
         magnet_hash = self._extract_hash(chosen_url)
-        # Cleaned_data contains the data array without the magnet urls.
-        # To use that, change cleaned_data to data
-        cleaned_data, chosen = self._remove_urls(data, chosen_url)
         data = json.dumps(data)
         now = datetime.datetime.today()
         date = now.isoformat()
@@ -36,36 +33,21 @@ class Tracking(DB):
 
         sql = '''
             INSERT INTO tracking(
-                download_date, show_title, season, episode, download_data, chosen, chosen_hash, one_off)
+                download_date, show_title, season, episode, chosen, chosen_hash, one_off)
             VALUES(
-                :date, :show_title, :season, :episode, :data, :chosen, :hash, :one_off);'''
+                :date, :show_title, :season, :episode, :chosen, :hash, :one_off);'''
 
         values = {
             'date': date,
             'show_title' : show_title,
             'season': season,
             'episode': episode,
-            'data': '',
-            'chosen': chosen,
+            'chosen': chosen_url,
             'hash': magnet_hash,
             'one_off': oneoff,
         }
 
         self.run_sql(sql, values)
-
-    def _remove_urls(self, data, chosen):
-        '''Remove the magnet url's from the data since they have no use
-        for data analysis and they take up to much room in the db
-        '''
-        return data, chosen
-
-        for i in range(len(data[1])):
-            url = data[1][i][4]
-            if url == chosen:
-                chosen = i
-            data[1][i][4] = i
-
-        return data, chosen
 
     def _extract_hash(self, url):
         if not url.startswith('magnet:'):
